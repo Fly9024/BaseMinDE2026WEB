387ec78ec98b6dd539d5ccd6deff6c19df7cdea5
fly9024 <fly9024@gmail.com>
2026-02-09 11:38:24 +0300

    Добавили регистрацию

---
+++ BaseMinDE2026WEB/Pages/RegistrationPage.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BaseMinDE2026WEB.Models;
+
+namespace BaseMinDE2026WEB.Pages
+{
+    public class RegistrationPageModel(DeWeb2026Context db) : PageModel
+    {
+        [BindProperty]//чтобы мы могли сделать привязку
+        //веделяем память не только под логин, но под объект с данными о пользователе
+        public LoginTable NewUser { get; set; } 
+            = new LoginTable() { UserTable = new UserTable() };
+
+        public void OnGet()
+        {
+        }
+
+
+        public IActionResult OnPost() 
+        {
+            db.LoginTables.Add(NewUser);
+            db.SaveChanges();
+            return RedirectToPage("/Index");
+        
+        }
+
+        public IActionResult OnGetLoginValidate(string login)//обработчик запроса на получение логина
+            => Content(db.LoginTables //возвращаем текст
+                .Select(x => x.Login) //делаем проекцию к столбцу с логином
+                .Contains(login) //проверяем, содержится ли переданный логин в столбце с логинами в базе
+                .ToString()); //переводим логическое значение в текст
+    }
+}
