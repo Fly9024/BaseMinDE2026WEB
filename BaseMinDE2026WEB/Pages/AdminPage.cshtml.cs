ed047d6afe2d82975d48e3e8e2a5c2f611d9c2b4
fly9024 <fly9024@gmail.com>
2026-02-16 11:01:42 +0300

    Сделали вывод заявок и изменение статуса заявки администратором

--- BaseMinDE2026WEB/Pages/AdminPage.cshtml.cs
+++ BaseMinDE2026WEB/Pages/AdminPage.cshtml.cs
@@ -1,14 +1,35 @@
+using BaseMinDE2026WEB.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BaseMinDE2026WEB.Pages
 {
     [Authorize(Roles ="Admin")]
-    public class AdminPageModel : PageModel
+    public class AdminPageModel(DeWeb2026Context db) : PageModel
     {
+        public List<OrderStatusTable> OrderStatuses => db.OrderStatusTables.ToList();
+
+        public List<OrderTable> UserOrders => db.OrderTables
+           .Include(x => x.IdCourseNavigation)
+           .Include(x => x.IdPaymentTypeNavigation)
+           .Include(x => x.IdStatusNavigation)          
+           .ToList();
         public void OnGet()
         {
+        }
+
+        public void OnGetChangeStatus(int idOrder, int idStatus)
+        {
+            //находим заявку по известному id
+            OrderTable order = db.OrderTables.FirstOrDefault(x => x.IdOrder == idOrder);
+            //меняем статус заявки
+            order.IdStatus = idStatus;
+            //очищаем поле с отзывом при изменении статуса
+            order.Reviev = null;         
+            //сохраняем изменения в базе
+            db.SaveChanges();
         }
     }
 }
